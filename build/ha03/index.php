3e0b1273fa2c02419b4b4f7d4a9902bb45d05b86
laurids <laurids.duellmann@gmail.com>
2015-11-26 22:42:43 +0100

    fixed wrapper width on HA03

--- build/ha03/index.php
+++ build/ha03/index.php
@@ -15,20 +15,23 @@
           <li><img src="/assets/images/ha03/celsius_times.svg" alt="°C Times"></li>
         </ul>
 
-        <div class="dt-ha03-content dt-row has-padding">
-          <div class="dt-col">
-            <div class="dt-ha03-info">
-              <h1>Celsius-Symbol</h1>
-              <p>&amp;#8451; <span>/</span> U+2103</p>
+        <div class="dt-wrapper">
+
+          <div class="dt-ha03-content dt-row has-padding">
+            <div class="dt-col">
+              <div class="dt-ha03-info">
+                <h1>Celsius-Symbol</h1>
+                <p>&amp;#8451; <span>/</span> U+2103</p>
+              </div>
             </div>
-          </div>
-          <div class="dt-col">
-            <div class="dt-ha03-about">
-              <p>Das Celcius Zeichen existiert als eigenständiges Unicode-Zeichen. Die generelle Empfehlung lautet jedoch, aus kompatibilitätsgründen nicht zu verwenden.</p>
+            <div class="dt-col">
+              <div class="dt-ha03-about">
+                <p>Das Celcius Zeichen existiert als eigenständiges Unicode-Zeichen. Die generelle Empfehlung lautet jedoch, aus kompatibilitätsgründen nicht zu verwenden.</p>
 
-              <p>Weder Futura, Frutiger, DIN Pro, Arial, Open Sans oder PT&nbsp;Sans unterstützen das Zeichen.</p>
+                <p>Weder Futura, Frutiger, DIN Pro, Arial, Open Sans oder PT&nbsp;Sans unterstützen das Zeichen.</p>
 
-              <p><img src="/assets/images/ha03/mini_c.svg" alt="°C American Typewriter">American Typewriter hingegen schon ;)</p>
+                <p><img src="/assets/images/ha03/mini_c.svg" alt="°C American Typewriter">American Typewriter hingegen schon ;)</p>
+              </div>
             </div>
           </div>
         </div>
