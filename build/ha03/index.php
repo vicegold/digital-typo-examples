512583b25730729c4bc56fdb25f705a99d2d7d3e
laurids <laurids.duellmann@gmail.com>
2015-11-26 21:26:05 +0100

    started on HA03

---
+++ build/ha03/index.php
@@ -0,0 +1,21 @@
+<?php include('../includes/head.php'); ?>
+    <title>HA03: Seltene Details und neue Tricks (Responsive)</title>
+
+  </head>
+
+  <body class="dt-body-ha03">
+
+    <div class="dt-wrapper">
+
+      <section class="dt-ha03">
+        <ul class="dt-ha03-symbols unstyled">
+          <li><img src="/assets/images/ha03/celsius_helvetica.svg" alt="°C Helvetica"></li>
+          <li><img src="/assets/images/ha03/celsius_avenir.svg" alt="°C Avenir"></li>
+          <li><img src="/assets/images/ha03/celsius_georgia.svg" alt="°C Georgia"></li>
+          <li><img src="/assets/images/ha03/celsius_times.svg" alt="°C Times"></li>
+        </ul>
+      </section>
+
+    </div>
+  </body>
+</html>
